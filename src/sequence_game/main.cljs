373ee429e51478eae8254b490c26c278a1170be2
Rafael Araujo <rafa.pc2112@gmail.com>
2020-05-03 17:55:44 -0300

    Adding base filer to work

--- src/sequence_game/main.cljs
+++ src/sequence_game/main.cljs
@@ -1,10 +1,44 @@
-(ns sequence-game.main)
+(ns sequence-game.main
+  (:require ["electron" :as e :refer (app BrowserWindow)]
+            ["path" :as path]
+            ["os"   :as os]
+            ["url"  :as url]))
+
+(defonce ^:private win-ref (atom nil))
+
+(defn- create-window []
+  (let [win
+        (BrowserWindow.
+          #js {:witdh 800
+               :height 600
+               :webPreferences
+               #js {:nodeIntegration true}})
+        url
+        (url/format #js {:pathname (path/join js/__dirname "index.html")
+                         :protocol "file:"
+                         :slashes true})]
+
+    (.loadURL win url)
+    (reset! win-ref win)
+    (.on win "closed"
+         (fn [e]
+           (reset! win-ref nil)))))
+
+(defn- maybe-quit []
+  (when (not= js/process.plataform "darwin")
+    (.quit app)))
+
+(defn- maybe-create-window []
+  (when-not @win-ref
+    (create-window)))
 
 (defn main []
-  (prn "OLAR"))
+  (.on app "ready"    create-window)
+  (.on app "activate" maybe-create-window)
+  (.on app "window-all-closed" maybe-quit))
 
 (defn start []
-  (prn "START"))
+  (js/console.log "Starting app"))
 
 (defn stop []
-  (prn "STOP"))
+  (js/console.log "Stoping app"))
--- src/sequence_game/renderer.cljs
+++ src/sequence_game/renderer.cljs
@@ -1,4 +1,33 @@
-(ns sequence-game.renderer)
+(ns sequence-game.renderer
+  (:require [reagent.core :as r]
+            [reagent.dom  :as rdom]))
 
-(defn main []
-  (prn "OLAR"))
+(def clicked-state (r/atom 0))
+
+(defn- clicked-action []
+  (swap! clicked-state inc)
+  (js/console.log "CLICKED"))
+
+(defn hello-html []
+  [:div
+    "The " [:code "clicked-qtd"] " has value: " @clicked-state ". "
+   [:button
+    {:value "CLICK ME"
+     :on-click clicked-action}
+    "!CLICK ME MATE!"]])
+
+(defn start []
+  (js/console.log "Starting application")
+  (rdom/render
+     [hello-html]
+     (.getElementById js/document "app")))
+
+(defn ^:dev/after-load after []
+  (start))
+
+(defn init []
+  (js/console.log "renderer - init")
+  (start))
+
+(defn stop []
+  (js/console.log "renderer - stop"))
